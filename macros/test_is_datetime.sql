ac1a242244400ac04e40dbe3cc68fd2bec159621
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-10 19:10:20 +0900

    fix error

--- macros/test_is_datetime.sql
+++ macros/test_is_datetime.sql
@@ -1,8 +1,8 @@
-{% macro test_is_datetime(model, column_name) %}
+{% test is_datetime(model, column_name) %}
 
   SELECT COUNT(*)
   FROM {{ model }}
   WHERE {{ column_name }} IS NOT NULL
-    AND EXTRACT(YEAR FROM {{ column_name }}) IS NULL
+    AND SAFE.EXTRACT(YEAR FROM {{ column_name }}) IS NULL
 
-{% endmacro %}
+{% endtest %}
